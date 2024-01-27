25cfe38c9ea8f36e1844c260dcd1d04d29bac676
Adyota Gupta <gupta.adyota@berkeley.edu>
2024-01-27 16:19:41 -0500

    simplified run script; still needs to be cleaned up

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,69 +5,65 @@ export MU=0.1
 export R=0.05
 export KN=400000
 export KT=200000
-export KS=300000
+export PRESSURE=350
+export KS=1000
 export M=40
 
-export INT_TIME=0.08
+# Loading conditions
+export CA=0
+export LOAD=100
+export INT_TIME=0.04
+
+# execution flags
+export CLEAN=false
 
 # Paths of interest
 export POST_PATH_BASE="/code/post"
 export IMG_PATH_BASE="/code/images"
 
+# Paths to save data [hc == hydrostatic constraint | nhc == no hydrostatic constraint]
+STRESS_PATH="$POST_PATH_BASE/${PRESSURE}/${LOAD}_${CA}/"
+STRESS_IMG="$IMG_PATH_BASE/${PRESSURE}/${LOAD}_${CA}/"
+
 # Explore parameter space (Load, Configuration Angle)
-load_start=50
-load_end=200
-load_step=25
+echo "(Load = $LOAD, Configuration Angle = $CA) [PRESSURE = ${PRESSURE}]"
 
-ca_start=10 # degrees
-ca_end=50
-ca_step=5
+# clear if it exists, and make new folder in its place
+if [ "$CLEAN" = true ]; then
+  rm -rf $POST_PATH_BASE
+  rm -rf $IMG_PATH_BASE
+  mkdir -p $POST_PATH_BASE
+  mkdir -p $IMG_PATH_BASE
 
-load_current=$load_start
-while (( $(bc <<< "$load_current < $load_end") )); do
-    ca_current=$ca_start
-    while (( $(bc <<< "$ca_current < $ca_end") )); do
-        echo "(Load = $load_current, Configuration Angle = $ca_current) [MU = $MU]"
+  rm -rf $STRESS_PATH
+  rm -rf $STRESS_IMG
+  mkdir -p $STRESS_PATH
+  mkdir -p $STRESS_IMG
 
-        # Paths to save data
-        STRESS_RISE_PATH="$POST_PATH_BASE/rise/${MU}/${load_current}_${ca_current}/"
-        STRESS_DROP_PATH="$POST_PATH_BASE/drop/${MU}/${load_current}_${ca_current}/"
-        STRESS_RISE_IMG="$IMG_PATH_BASE/rise/${MU}/${load_current}_${ca_current}/"
-        STRESS_DROP_IMG="$IMG_PATH_BASE/drop/${MU}/${load_current}_${ca_current}/"
+fi
 
-        # clear if it exists, and make new folder in its place
-        # rm -rf $STRESS_RISE_PATH
-        # rm -rf $STRESS_DROP_PATH
-        rm -rf $STRESS_RISE_IMG
-        rm -rf $STRESS_DROP_IMG
+# first solve for initial conditions
+python -u /code/src/initCond.py -p $STRESS_PATH -a $CA -hp $PRESSURE
 
-        # mkdir -p $STRESS_RISE_PATH
-        # mkdir -p $STRESS_DROP_PATH
-        mkdir -p $STRESS_RISE_IMG
-        mkdir -p $STRESS_DROP_IMG
+# generate model
+python -u /code/src/toymodel.py -p $STRESS_PATH -l $LOAD -a $CA -hp $PRESSURE
 
-        # # run stress rise case
-        # python -u /code/src/toymodelRISE.py -p $STRESS_RISE_PATH -l $load_current -a $ca_current # generate functions
-        #
-        # integrate the ODE and save solution; if it fails, creates a fail file
-        # if python -u /code/src/odesolve.py -p $STRESS_RISE_PATH -a $ca_current -d '1E-8'; then
-        #     :
-        # else
-        #     echo "WARNING: Integration failed! Lower integration time?"
-        #     echo "$INT_TIME" > $STRESS_RISE_PATH/fail.txt
-        # fi
+# run constrained case
+python -u /code/src/odesolve.py -p $STRESS_RISE -a $CA  --maintain
 
-        # postprocess stress rise
-        python -u /code/src/postprocessRISE.py -p $STRESS_RISE_PATH -a $ca_current -i $STRESS_RISE_IMG
-
-        # now run stress drop that follows
+exit 0
 
 
-        # postprocess stress drop
+#integrate the ODE and save solution; if it fails, creates a fail file
+if python -u /code/src/odesolve.py -p $STRESS_RISE_PATH -a $CA  --maintain; then
+    :
+else
+    echo "WARNING: Integration failed! Lower integration time?"
+    echo "$INT_TIME_RISE" > $STRESS_RISE_PATH/fail.txt
+fi
 
-        break
-        ca_current=$(bc <<< "$ca_current + $ca_step")
-    done
-    load_current=$(bc <<< "$load_current + $load_step")
-    break
-done
+
+# run post processing on the stress rise
+python -u /code/src/postprocessRISE.py  -p $STRESS_RISE_PATH -i $STRESS_RISE_IMG -a $CA
+
+exit 0
