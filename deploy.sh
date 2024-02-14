122083d76fce60180718819295d44227688accff
agupta87 <gupta.adyota@berkeley.edu>
2024-02-14 15:38:29 -0500

    simplified cases tested

--- deploy.sh
+++ deploy.sh
@@ -10,42 +10,23 @@ echo "Creating model...."
 #docker wait toy_model_CREATE
 
 echo "Running models...."
-ALL_LOADS=(490)
+ALL_LOADS=490
+ALL_KS=6000
 HYDROSTATIC=(4460 4560 4660)
-ALL_KS=(6000)
-ALL_KS_RATE=(-1000 -500 -100 100 500 1000)
+ALL_KS_RATE=(-1000 1000)
 
-## vary LOAD
-#for l in ${ALL_LOADS[@]};
-#do
-#    echo "toy_model_LOAD_${l}"
-#    docker run -e LOAD=${l} -e PRESSURE=${HYDROSTATIC[1]} -e KS_START=${ALL_KS[2]} -e KS_RATE_RISE=${ALL_KS_RATE[3]} -d --rm --name "toy_model_LOAD_${l}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
-#done
-#
-#
-## vary PRESSURE
-#for h in ${HYDROSTATIC[@]};
-#do
-#    echo "toy_model_HYDROSTATIC_${h}"
-#    docker run -e LOAD=${ALL_LOADS[2]} -e PRESSURE=${h} -e KS_START=${ALL_KS[2]} -e KS_RATE_RISE=${ALL_KS_RATE[3]} -d --rm --name "toy_model_HYDROSTATIC_${h}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
-#done
-#
-#
-## vary KS value
-#for ks in ${ALL_KS[@]};
-#do
-#    echo "toy_model_ALL_KS_${ks}"
-#    docker run -e LOAD=${ALL_LOADS[2]} -e PRESSURE=${HYDROSTATIC[1]} -e KS_START=${ks} -e KS_RATE_RISE=${ALL_KS_RATE[3]} -d --rm --name "toy_model_ALL_KS_${ks}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
-#done
-
-
-# vary KS rate
-for ksr in ${ALL_KS_RATE[@]};
+# vary PRESSURE and KS rate
+for h in ${HYDROSTATIC[@]};
 do
-    echo "toy_model_ALL_KS_RATE_${ksr}"
-    #docker run -e LOAD=${ALL_LOADS[0]} -e PRESSURE=${HYDROSTATIC[1]} -e KS_START=${ALL_KS[0]} -e KS_RATE_RISE=${ksr} -it --rm --name "toy_model_ALL_KS_RATE_${ksr}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
-    #break
-    docker run -e LOAD=${ALL_LOADS[0]} -e PRESSURE=${HYDROSTATIC[1]} -e KS_START=${ALL_KS[0]} -e KS_RATE_RISE=${ksr} -d --rm --name "toy_model_ALL_KS_RATE_${ksr}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
+	for ksr in ${ALL_KS_RATE[@]};
+	do
+    		echo "toy_model_${ksr}_${h}"
+		#docker run -e LOAD=${ALL_LOADS[0]} -e PRESSURE=${HYDROSTATIC[1]} -e KS_START=${ALL_KS[0]} -e KS_RATE_RISE=${ksr} -it --rm --name "toy_model_ALL_KS_RATE_${ksr}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
+		#break
+		docker run -e LOAD=${ALL_LOADS} -e PRESSURE=${h} -e KS_START=${ALL_KS} -e KS_RATE_RISE=${ksr} \
+			-d --rm --name "M_toy_model_${h}_${ksr}" -v $PWD:/code adyota/toymodel "/code/scripts/run.sh"
+
+	done
 done
 
 
